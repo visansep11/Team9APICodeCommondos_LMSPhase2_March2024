ba1a5d7456a6406dbf420733f5d0291cc93a9b7b
sanka <sanka@DESKTOP-KINS3OS.attlocal.net>
2024-03-31 17:10:04 -0400

    Recent Files

--- src/test/java/com/LMSAPI/Authentication/BearerAuth.java
+++ src/test/java/com/LMSAPI/Authentication/BearerAuth.java
@@ -1,26 +1,70 @@
 package com.LMSAPI.Authentication;
 
-import com.LMSAPI.Utilities.RestAPIHelper;
+//import com.LMSAPI.Utilities.RestAPIHelper;
+import com.jayway.jsonpath.JsonPath;
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
+import com.LMSAPI.Utilities.FileNameConstants;
+
+//import com.testautomation.apitesting.listener.RestAssuredListener;
 
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import com.LMSAPI.Endpoints.LMSAPI_EndPoints;
+import com.LMSAPI.Utilities.ConfigReaderAndWriter;
+
 
 public class BearerAuth {
-	@Before
-	 public static void basicAuthAPITest() {
-		  
-		  RestAssured
-		  		.given()
-		  			.spec(RestAPIHelper.getBearer())
-		  			.contentType(ContentType.JSON)
-		  			.baseUri("https://lms-marchapi-hackathon-a258d2bbd43b.herokuapp.com/lmsswagger-ui.html#/")
-		  		.when()
-		  			.get()
-		  		.then()
-		  			.assertThat()
-		  			.statusCode(200);
-		  
+//	ConfigReaderAndWriter configReaderObj;
+//	static Properties prop; 
+//	
+//    public BearerAuth() {
+//    	//pass object through constructor
+//    	configReaderObj = new ConfigReaderAndWriter();
+//    	 prop = configReaderObj.init_prop();
+//    }
+	 public static String BearerAuthAPITest() {
+		
+		String tokenAPIRequestBody = null;
+		try {
+			tokenAPIRequestBody = FileUtils.readFileToString(new File(FileNameConstants.TOKEN_API_REQUEST_BODY),
+					"UTF-8");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// token generation
+		System.out.println("tokenAPIRequestBody :" + tokenAPIRequestBody );
+					Response tokenAPIResponse = RestAssured
+							.given()
+							//.spec(RestAPIHelper.getBearer())
+//								.filter(new RestAssuredListener())
+								.contentType(ContentType.JSON)
+								.body(tokenAPIRequestBody)
+//								.baseUri("https://lms-marchapi-hackathon-a258d2bbd43b.herokuapp.com/lms/login")
+								
+							.baseUri(LMSAPI_EndPoints.User_Login)
+							.when()
+								.post()
+							.then()
+								.assertThat()
+								.statusCode(200)
+							.extract()
+								.response();
+
+					String token = JsonPath.read(tokenAPIResponse.body().asString(), "$.token");
+					String authtoken = "Bearer "+token;
+					System.out.println("Token Id : " + authtoken);
+					return authtoken;
 	  }
 	  
 	}
---
+++ src/test/java/com/LMSAPI/Utilities/FileNameConstants.java
@@ -0,0 +1,26 @@
+package com.LMSAPI.Utilities;
+//import java.util.Properties;
+//import com.LMSAPI.Utilities.*;
+
+
+public class FileNameConstants {
+		
+	public static final String BASE_PATH ="src/test/resources/";
+//	public static final String excelFilePath= "src/test/resources/Testdata/LMS.xlsx";
+
+	
+	public static final String TOKEN_API_REQUEST_BODY = BASE_PATH+"tokenapirequestbody.txt";
+	public static final String CSV_TEST_DATA =BASE_PATH+"testdatacsv.csv";
+//	public static final String PUT_API_REQUEST_BODY = BASE_PATH+"putapirequestbody.txt";
+//	
+//	public static final String PATCH_API_REQUEST_BODY = BASE_PATH+"patchapirequestbody.txt";
+//	
+//	public static final String JSON_TEST_DATA = BASE_PATH+"testdatajson.json";
+//	
+//	public static final String CSV_TEST_DATA = BASE_PATH+"testdatacsv.csv";
+//	
+//	public static final String EXCEL_TEST_DATA = BASE_PATH+"exceltestdata.xlsx";
+//	
+//	public static final String TEST_RUNNER = BASE_PATH+"TestRunner.xlsx";
+//	
+}
